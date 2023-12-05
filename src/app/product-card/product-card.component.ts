d43bba6fe81ec06bbc9db4a49c0a3a7b219e7c09
Samet YUKSEL <sametmertyukselmlg@gmail.com>
2023-12-05 13:00:22 +0300

    Add ProductDetails to Product Card Component

--- src/app/product-card/product-card.component.ts
+++ src/app/product-card/product-card.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { ProductDetails } from '../product-details';
 
 @Component({
   selector: 'app-product-card',
   templateUrl: './product-card.component.html',
-  styleUrls: ['./product-card.component.css']
+  styleUrls: ['./product-card.component.css'],
 })
 export class ProductCardComponent {
-
+  @Input() productDetails?: ProductDetails;
 }
