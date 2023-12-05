8370963d8667abb0eed319940dc28f4abc60befd
Samet YUKSEL <sametmertyukselmlg@gmail.com>
2023-12-05 13:02:34 +0300

    Add ProductService to Products Grid Component

--- src/app/products-grid/products-grid.component.ts
+++ src/app/products-grid/products-grid.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { ProductDetails } from '../product-details';
+import { ProductService } from '../services/product.service';
 
 @Component({
   selector: 'app-products-grid',
   templateUrl: './products-grid.component.html',
-  styleUrls: ['./products-grid.component.css']
+  styleUrls: ['./products-grid.component.css'],
 })
 export class ProductsGridComponent {
+  products: ProductDetails[] = [];
+  constructor(private productService: ProductService) {}
 
+  ngOnInit(): void {
+    this.getProducts();
+  }
+
+  getProducts() {
+    this.productService
+      .getProducts()
+      .subscribe((response) => (this.products = response.data));
+  }
 }
