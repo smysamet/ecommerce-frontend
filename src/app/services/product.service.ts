5d04080c45abb76e5f1e99bcd68e74dc551829ad
Samet YUKSEL <sametmertyukselmlg@gmail.com>
2023-12-05 13:01:44 +0300

    Add getProducts Method to Product Service

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { ProductDetails } from '../product-details';
+import { Observable, of } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { GetProductsResponse } from '../get-products-response';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductService {
+  private produtApiUrl = 'http://localhost:8080/api/products';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getProducts(): Observable<GetProductsResponse> {
+    return this.http.get<GetProductsResponse>(this.produtApiUrl);
+  }
 }
