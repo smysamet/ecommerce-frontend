5d7943b65afd4c2e4bdfc69a481232c2793698ec
Samet YUKSEL <sametmertyukselmlg@gmail.com>
2023-12-05 11:59:16 +0300

    Add HttpClientModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,13 +13,10 @@ import { ProductCardComponent } from './product-card/product-card.component';
     AppComponent,
     NavbarComponent,
     ProductsGridComponent,
-    ProductCardComponent
+    ProductCardComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
